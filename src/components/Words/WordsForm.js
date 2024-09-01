d2e7ccaec94c17b190656aed6dae54de8e6050e3
Rosen Bahnev <bahnev@gmail.com>
2024-09-01 11:08:29 +0300

    add spinner for the new word

--- src/components/Words/WordsForm.js
+++ src/components/Words/WordsForm.js
@@ -2,6 +2,7 @@ import styles from "./Words.module.css";
 import { useForm } from "react-hook-form";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { addWord } from "../../services.js/apiWords";
+import SmalSpinner from "../Spinner/SmalSpinner";
 
 export default function WordsForm() {
     const queryClient = useQueryClient();
@@ -9,13 +10,15 @@ export default function WordsForm() {
     const { register, handleSubmit, reset, getValues, formState } = useForm();
     const { errors } = formState;
 
-    const { mutate, isLoading: isAdding } = useMutation({
+    const { mutate, isPending: isAdding } = useMutation({
         mutationFn: addWord,
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ["words"] });
             reset();
         },
     });
+
+    console.log(isAdding);
 
     function onSubmit(data) {
         mutate(data);
@@ -39,7 +42,9 @@ export default function WordsForm() {
             <div className={styles.errorDiv}>
                 {errors?.word && <p className="inputError">Невалидна дума</p>}
             </div>
-            <button className="btn-general">Добави дума</button>
+            <button className="btn-general" disabled={isAdding}>
+                Добави дума {isAdding ? <SmalSpinner /> : ""}
+            </button>
         </form>
     );
 }
